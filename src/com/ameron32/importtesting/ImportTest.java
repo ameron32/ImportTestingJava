c8d5b828e015933a359309a109ab6d44c0a70120
Kris L <ameron32extras@gmail.com>
2013-06-25 17:05:42 -0500

    cleanup imports

--- src/com/ameron32/importtesting/ImportTest.java
+++ src/com/ameron32/importtesting/ImportTest.java
@@ -1,19 +1,6 @@
 
 package com.ameron32.importtesting;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.ameron32.libgurps.attackoptions.AttackOption;
-import com.ameron32.libgurps.attackoptions.MeleeAttackOption;
-import com.ameron32.libgurps.items.design.Item;
-import com.ameron32.libgurps.items.design.Weapon;
-import com.ameron32.libgurps.tools.Importer;
-import com.ameron32.libgurps.tools.Importer.ImportType;
 import com.ameron32.testing.ImportTesting;
 
 public class ImportTest {

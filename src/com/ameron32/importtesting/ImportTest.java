17e3d37da3544320ea0a5a68da94f99bb784c32f
Kris L <ameron32extras@gmail.com>
2013-06-26 16:48:36 -0500

    temp input option, correction of directory

--- src/com/ameron32/importtesting/ImportTest.java
+++ src/com/ameron32/importtesting/ImportTest.java
@@ -1,6 +1,8 @@
 
 package com.ameron32.importtesting;
 
+import java.util.Scanner;
+
 import com.ameron32.testing.ImportTesting;
 
 public class ImportTest {
@@ -9,11 +11,23 @@ public class ImportTest {
     public static void main(String[] args) {
         it = new ImportTesting(
                 new String[] {
-                        "C:\\Users\\klemeilleur\\Dropbox\\Public\\GURPS\\"
+                        "C:\\Users\\klemeilleur\\Dropbox\\Public\\GURPS\\GURPSBuilder\\"
                 }
                 );
         it.main();
         System.out.println(ImportTesting.getSB());
     }
+    
+    /* LOOPABLE INPUT */
+    private static void input() {
+        String next = "";
+        while (in.hasNext()) { 
+            next = in.next();
+
+            // perform loop
+        }
+        
+    }
+    static Scanner in = new Scanner(System.in);
 
 }
